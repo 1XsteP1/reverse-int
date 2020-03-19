acd7dda59292b4cee924821ab815decf20138d4c
1XsteP1 <60567042+1XsteP1@users.noreply.github.com>
2020-03-19 17:36:53 +0200

    feat: add correct solution

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-module.exports = function reverse (n) {
-  if (n < 0) {
-      n = n * -1;
-  }
-    
-  let number = n.toString().toSplit('').reverse().join('');
-  return number
-}
+module.exports = function reverse(n) {
+    if (n < 0) n = n * -1;
+
+    let numb = n.toString().split("").reverse().join("");
+
+    return numb;
+};
